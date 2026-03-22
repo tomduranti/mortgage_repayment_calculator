a53b71cc6c0ab699ea71dd425188ded73fe67d42
tommaso.duranti191@gmail.com <tommaso.duranti191@gmail.com>
2026-03-22 21:10:14 +0100

    Complete desktop design

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ export default function App() {
     <main className='page_wrapper'>
       <div className='mortgage_repayment'>
 
-        <div className="mortgage_repayment__calculator  container">
+        <div className="mortgage_repayment__calculator">
           <div className='mortgage_repayment__title'>
             <h1 className='mortgage_repayment__heading  text_preset_2  text_capitalize'>mortgage calculator</h1>
             <ButtonReset />
@@ -29,7 +29,7 @@ export default function App() {
         </div>
 
         <div className='mortgage_repayment__result'>
-          <div className="mortgage_repayment__container  container">
+          <div className="mortgage_repayment__container">
             <img className='mortgage_repayment__icon' src={EmptyIcon} alt='' />
             <h3 className='text_white  text_preset_2'>Results shown here</h3>
             <p className='text_slate--300  text_preset_4'>Complete the form and click “calculate repayments” to see what your monthly repayments would be.</p>
