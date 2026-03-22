2bf4f966e87524eadda317c3c80ae5e87c9de1ba
tommaso.duranti191@gmail.com <tommaso.duranti191@gmail.com>
2026-03-22 17:01:35 +0100

    Complete mobile design

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import ButtonReset from './components/atoms/Button/ButtonReset.jsx';
 import Input from './components/atoms/Input/Input.jsx';
 import MortgageTypeRadio from './components/molecules/MortgageTypeRadio/MortgageTypeRadio.jsx';
 
+import EmptyIcon from './assets/illustration_empty.svg';
+
 import './sass/main.scss';
 import './sass/base/_base.scss';
 
@@ -10,8 +12,8 @@ export default function App() {
 
   return (
     <main>
-      <div className='mortgage_repayment  container'>
-        <div className="mortgage_repayment__calculator">
+      <div className='mortgage_repayment'>
+        <div className="mortgage_repayment__calculator  container">
           <div className='mortgage_repayment__title'>
             <h1 className='mortgage_repayment__heading  text_preset_2  text_capitalize'>mortgage calculator</h1>
             <ButtonReset />
@@ -23,7 +25,13 @@ export default function App() {
           <ButtonRepayment />
         </div>
       
-        <div className="mortgage_repayment__result"></div>
+        <div className='mortgage_repayment__result'>
+          <div className="mortgage_repayment__container  container">
+            <img className='mortgage_repayment__icon' src={EmptyIcon} alt='' />
+            <h3 className='mortgage_repayment__empty_text  text_preset_2'>Results shown here</h3>
+            <p className='mortgage_repayment__paragraph  text_preset_4'>Complete the form and click “calculate repayments” to see what your monthly repayments would be.</p>
+          </div>
+        </div>
       </div>
     </main>
   )
