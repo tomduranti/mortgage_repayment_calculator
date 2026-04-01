bc8d95d0c79dc820e6143c71129c4860a700fc16
tommaso.duranti191@gmail.com <tommaso.duranti191@gmail.com>
2026-04-02 00:18:00 +0200

    Add modular sass for _base.scss and use modular classes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import EmptyIcon from './assets/illustration_empty.svg';
 import MortgageRepayment from './MortgageRepayment.js';
 
 import './sass/main.scss';
-import './sass/base/_base.scss';
+import styles from './sass/base/_Base.module.scss';
 
 export default function App() {
 
@@ -34,16 +34,16 @@ export default function App() {
 
 
   return (
-    <main className='page_wrapper'>
-      <div className='mortgage_repayment'>
+    <main className={`${styles.page_wrapper}`}>
+      <div className={`${styles.mortgage_repayment}`}>
 
-        <div className='mortgage_repayment__calculator'>
-          <div className='mortgage_repayment__title'>
+        <div className={`${styles.mortgage_repayment__calculator}`}>
+          <div className={`${styles.mortgage_repayment__title}`}>
             <h1 className='text_preset_2  text_capitalize'>mortgage calculator</h1>
             <ButtonReset onClick={() => resetState(initialState)} />
           </div>
 
-          <form onSubmit={handleSubmit(setMortgageRepayment)} className='form' noValidate>
+          <form onSubmit={handleSubmit(setMortgageRepayment)} className={`${styles.form}`} noValidate>
 
             <div>
               <Controller
@@ -60,7 +60,7 @@ export default function App() {
                   />
                 )}
               />
-              <span className='error  text_preset_5  text_red'>{errors.mortgage_amount?.message}</span>
+              <span className={`${styles.error} text_preset_5  text_red`}>{errors.mortgage_amount?.message}</span>
             </div>
 
             <div>
@@ -77,7 +77,7 @@ export default function App() {
                   />
                 )}
               />
-              <span className='error  text_preset_5  text_red'>{errors.mortgage_term?.message}</span>
+              <span className={`${styles.error} text_preset_5  text_red`}>{errors.mortgage_term?.message}</span>
             </div>
 
             <div>
@@ -94,10 +94,10 @@ export default function App() {
                   />
                 )}
               />
-              <span className='error  text_preset_5  text_red'>{errors.interest_rate?.message}</span>
+              <span className={`${styles.error} text_preset_5  text_red`}>{errors.interest_rate?.message}</span>
             </div>
 
-            <div className='form__radio  text_slate--700'>
+            <div className={`${styles.form__radio}  text_slate--700`}>
               <h2 className='text_preset_4  text_capitalize'>mortgage type</h2>
               <Radio {...register('mortgage_type', {
                 required: 'This field is required'
@@ -122,31 +122,31 @@ export default function App() {
 
         </div>
 
-        <div className='mortgage_repayment__result'>
-          <div className='mortgage_repayment__container'>
+        <div className={`${styles.mortgage_repayment__result}`}>
+          <div className={`${styles.mortgage_repayment__container}`}>
             {mortgage.repayment ?
               (
                 <>
                   <div>
-                    <h3 className='mortgage_repayment__heading  text_white  text_preset_2'>Your results</h3>
+                    <h3 className={`${styles.mortgage_repayment__heading} text_white  text_preset_2`}>Your results</h3>
                     <p className='text_slate--300  text_preset_4'>Your results are shown below based on the information you provided. To adjust the results, edit the form and click “calculate repayments” again.</p>
                   </div>
-                  <div className='inbox'>
+                  <div className={`${styles.inbox}`}>
 
                     {mortgage.type === 'repayment' ? (
-                      <div className='inbox__details'>
+                      <div className={`${styles.inbox__details}`}>
                         <h4 className='text_preset_4  text_slate--300'>Your monthly repayments</h4>
                         <span className='text_preset_1  text_lime'>£{setNumberFormat(mortgage.repayment.toFixed(2))}</span>
                       </div>
                     ) : (
-                      <div className='inbox__details'>
+                      <div className={`${styles.inbox__details}`}>
                         <h4 className='text_preset_4  text_slate--300'>Your monthly interest</h4>
                         <span className='text_preset_1  text_lime'>£{setNumberFormat(mortgage.monthly_interest.toFixed(2))}</span>
                       </div>
                     )}
 
                     <span className='separator'></span>
-                    <div className='inbox__details'>
+                    <div className={`${styles.inbox__details}`}>
                       <h4 className='text_preset_4  text_slate--300'>Total you'll repay over the term</h4>
                       <span className='text_preset_2  text_white'>£{setNumberFormat(mortgage.total_repayment.toFixed(2))}</span>
                     </div>
@@ -154,7 +154,7 @@ export default function App() {
                 </>
               ) : (
                 <>
-                  <img className='mortgage_repayment__icon' src={EmptyIcon} alt='' />
+                  <img className={`${styles.mortgage_repayment__icon}`} src={EmptyIcon} alt='' />
                   <h3 className='text_white  text_preset_2  text_center'>Results shown here</h3>
                   <p className='text_slate--300  text_preset_4  text_center'>Complete the form and click “calculate repayments” to see what your monthly repayments would be.</p>
                 </>
