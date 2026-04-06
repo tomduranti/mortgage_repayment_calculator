8fa6b7865651d4402fc0cfa4b400d0734201212d
tommaso.duranti191@gmail.com <tommaso.duranti191@gmail.com>
2026-04-07 00:31:07 +0200

    Fix div inside label error

--- src/components/atoms/Input/Input.jsx
+++ src/components/atoms/Input/Input.jsx
@@ -4,18 +4,17 @@ import { NumericFormat } from 'react-number-format';
 export default function Input({ label_text, prefix_text, dir, error, ...field }) {
     return (
         <>
-            <label>
-                <span className={`${styles.label__text}  text_preset_4  text_capitalize  text_slate--700`}>{label_text}</span>
-                <div className={`${styles.input_number} ${error && styles['input_number--error']}  text_preset_3`}>
-                    <NumericFormat
-                        value={field.value ?? ''}
-                        thousandSeparator
-                        onValueChange={values => field.onChange(values.floatValue)}
-                        className={`${styles.input_number__input} ${dir === 'r' && 'flex_order_right'}`}
-                        required
-                    />
-                    <span className={`${styles.input_number__prefix} ${styles['input_number__prefix--bg_lime']} ${error ? styles['input_number__prefix--error'] : 'text_slate--700'}`}>{prefix_text}</span>
-                </div>
+            <span className={`${styles.label__text}  text_preset_4  text_capitalize  text_slate--700`}>{label_text}</span>
+            <label className={`${styles.input_number} ${error && styles['input_number--error']}  text_preset_3`}>
+                <NumericFormat
+                    value={field.value ?? ''}
+                    name={label_text}
+                    thousandSeparator
+                    onValueChange={values => field.onChange(values.floatValue)}
+                    className={`${styles.input_number__input} ${dir === 'r' && 'flex_order_right'}`}
+                    required
+                />
+                <span className={`${styles.input_number__prefix} ${styles['input_number__prefix--bg_lime']} ${error ? styles['input_number__prefix--error'] : 'text_slate--700'}`}>{prefix_text}</span>
             </label>
         </>
     )
